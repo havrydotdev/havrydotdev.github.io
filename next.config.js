56fa10a8c82abae4b4207e1b6759c325d76ab828
Ivan <fistashkin0123@gmail.com>
2023-07-20 10:01:01 +0300

    v0.1

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+};
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig;
